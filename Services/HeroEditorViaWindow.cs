cbe147662a5f351ec6d9907dbd43d6ba6bad44e5
kissadin <kissadin@gmail.com>
2022-03-01 14:10:06 +0100

    Hero Editor

---
+++ Services/HeroEditorViaWindow.cs
@@ -0,0 +1,17 @@
+﻿using GUI_Lab04.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Lab04.Services
+{
+    class HeroEditorViaWindow : IHeroEditorService
+    {
+        public void Edit(Hero hero)
+        {
+            new HeroEditorWindow(hero).ShowDialog();
+        }
+    }
+}
---
+++ Services/IHeroEditorService.cs
@@ -0,0 +1,14 @@
+﻿using GUI_Lab04.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Lab04.Services
+{
+    public interface IHeroEditorService
+    {
+        void Edit(Hero hero);
+    }
+}
